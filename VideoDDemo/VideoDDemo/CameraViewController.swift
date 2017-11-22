e3cefd0561d69bbff4668683f49892432d667282
ArjunaPratama <32661531+ArjunaPratama@users.noreply.github.com>
2017-11-22 13:57:02 +0700

    FaceDtector

---
+++ VideoDDemo/VideoDDemo/CameraViewController.swift
@@ -0,0 +1,46 @@
+//
+//  CameraViewController.swift
+//  VideoDDemo
+//
+//  Created by Jun  on 11/22/17.
+//  Copyright © 2017 Arjuna. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnCamera(_ sender: Any) {
+        
+        var imagePicker: UIImagePickerController!
+        
+        imagePicker =  UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
